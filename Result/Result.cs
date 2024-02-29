5c53426691b624bfafa140a82e569dd64a0fbfd7
Yves Goeleven <yves@goeleven.com>
2024-02-29 23:46:58 +0100

    result monad

---
+++ Result/Result.cs
@@ -0,0 +1,94 @@
+﻿namespace Result
+{
+    public readonly struct Result<L, R>
+    {
+        private readonly R _right;
+        private readonly L _left;
+        private readonly bool _isRight;
+        private readonly bool _isLeft;
+
+        private Result(R right)
+        {
+            this._isRight = true;
+            this._isLeft = false;
+            this._right = right;
+            this._left = default;
+        }
+        private Result(L left)
+        {
+            this._isLeft = true;
+            this._isRight = false;
+            this._left = left;
+            this._right = default;
+        }
+
+        public static implicit operator Result<L, R>(R value) =>
+            new Result<L, R>(value);
+
+        public static implicit operator Result<L, R>(L value) =>
+            new Result<L, R>(value);
+
+        public static Result<L, R> From(R value) => new Result<L, R>(value);
+
+        public Result<L, R2> Map<R2>(Func<R, R2> map) =>
+            this._isRight
+                ? new Result<L, R2>(map(_right))
+                : new Result<L, R2>(_left);
+
+        public Result<L, R2> Bind<R2>(Func<R, Result<L, R2>> f) =>
+            this._isRight
+                ? f(_right)
+                : new Result<L, R2>(_left);
+       
+        public async Task<Result<L, R2>> BindAsync<R2>(Func<R, Task<Result<L, R2>>> bind) =>
+            this._isRight
+                ? await bind(this._right).ConfigureAwait(false)
+                : new Result<L, R2>(_left);
+
+        public T Fold<T>(Func<L, T> leftFunc, Func<R, T> rightFunc) =>
+           this._isRight
+               ? rightFunc(_right)
+               : leftFunc(_left);
+
+    }
+
+
+    public static class ResultExtension
+    {
+        public static Result<L, V> SelectMany<U, V, L, R>(this Result<L, R> first, Func<R, Result<L, U>> second, Func<R, U, V> project)
+        {
+            return first.Bind(a => second(a).Map(b => project(a, b)));
+        }
+        public static Result<L, U> Select<U, L, R>(this Result<L, R> first, Func<R, U> map) => first.Map(map);
+
+    }
+
+    public static class TaskExtension
+    {
+        public static Task<Result<L, R>> ToAsync<L, R>(this Result<L, R> result)
+        {
+            return Task.FromResult(result);
+        }
+
+        public static Task<Result<L, R>> ToAsync<L, R>(this R value)
+        {
+            return Task.FromResult(Result<L, R>.From(value));
+        }
+
+        public static async Task<Result<L, U>> Select<U, L, R>(this Task<Result<L, R>> first, Func<R, U> map) => (await first).Map(map);
+        public static async Task<Result<L, V>> SelectMany<U, V, L, R>(this Task<Result<L, R>> first, Func<R, Task<Result<L, U>>> second, Func<R, U, V> project)
+        {
+            return await (await first).BindAsync(async a => (await second(a)).Map(b => project(a, b)));
+        }
+
+        public static async Task<Result<L, V>> SelectMany<U, V, L, R>(this Result<L, R> first, Func<R, Task<Result<L, U>>> second, Func<R, U, V> project)
+        {
+            return await first.BindAsync(async a => (await second(a)).Map(b => project(a, b)));
+        }
+
+        public static async Task<Result<L, V>> SelectMany<U, V, L, R>(this Task<Result<L, R>> first, Func<R, Result<L, U>> second, Func<R, U, V> project)
+        {
+            return (await first).Bind(a => second(a).Map(b => project(a, b)));
+        }
+    }
+}
---
+++ Result/TestScenarios.cs
@@ -0,0 +1,121 @@
+using FluentAssertions;
+
+namespace Result
+{
+    public class TestScenarios
+    {
+        public Task<Result<Problem, Success>> HandleCommand(
+            Command command,
+            Func<Command, Result<Problem, ValidatedCommand>> validate,
+            Func<User, Result<Problem, Authorized>> authorize,
+            Func<ValidatedCommand, Task<Result<Problem, Event[]>>> load,
+            Func<Event[], ValidatedCommand, AggregateRoot> action,
+            Func<AggregateRoot, Task<Result<Problem, Event[]>>> persist)
+        {
+            return
+                from validatedCommand in validate(command)
+                from authorized in authorize(new User())
+                from history in load(validatedCommand)
+                let aggregateRoot = action(history, validatedCommand)
+                from emitted in persist(aggregateRoot)
+                select new Success
+                {                    
+                    Emitted = emitted
+                };
+        }
+
+        [Fact]
+        public async Task CanSucceed()
+        {
+            var result = await HandleCommand(new PlaceOrder(),
+                request => new ValidatedCommand(),
+                command => new Authorized(),
+                async command => new Event[0],
+                (events, command) => new Booking(),
+                async (booking) => new Event[0]);
+
+            result.Fold(
+                p => p.Should().BeNull(),
+                s => s.Should().BeOfType<Success>()
+            );
+        }
+
+        [Fact]
+        public async Task CanFailOnValidation()
+        {
+            var result = await HandleCommand(new PlaceOrder(),
+                request => new Problem(),
+                command => new Authorized(),
+                async command => new Event[0],
+                (events, command) => new Booking(),
+                async (booking) => new Event[0]);
+
+            result.Fold(
+               p => p.Should().BeOfType<Problem>(),
+               s => s.Should().BeNull()
+           );
+        }
+
+        [Fact]
+        public async Task CanFailOnAuthorization()
+        {
+            var result = await HandleCommand(new PlaceOrder(),
+                request => new ValidatedCommand(),
+                command => new Problem(),
+                async command => new Event[0],
+                (events, command) => new Booking(),
+                async ( booking) => new Event[0]);
+
+            result.Fold(
+               p => p.Should().BeOfType<Problem>(),
+               s => s.Should().BeNull()
+           );
+        }
+
+        [Fact]
+        public async Task CanFailOnLoading()
+        {
+            var result = await HandleCommand(new PlaceOrder(),
+                request => new ValidatedCommand(),
+                command => new Authorized(),
+                async command => new Problem(),
+                (events, command) => new Booking(),
+                async (booking) => new Event[0]);
+
+            result.Fold(
+               p => p.Should().NotBeNull(),
+               s => s.Should().BeNull()
+           );
+        }
+
+        [Fact]
+        public async Task CanFailOnPersistance()
+        {
+            var result = await HandleCommand(new PlaceOrder(),
+                request => new ValidatedCommand(),
+                command => new Authorized(),
+                async command => new Event[0],
+                (events, command) => new Booking(),
+                async (booking) => new Problem());
+
+            result.Fold(
+               p => p.Should().NotBeNull(),
+               s => s.Should().BeNull()
+           );
+        }       
+    }
+
+    public record PlaceOrder : Command { }
+    public record Command { }
+    public record ValidatedCommand { }
+    public record Problem { }
+    public record User { }
+    public record Authorized { }
+    public record Event { }
+    public record Booking : AggregateRoot { }
+    public record AggregateRoot { }
+    public record Success
+    {
+        public Event[] Emitted { get; internal set; }
+    }
+}
